c67776054f379cd82e9e664f0462cedec41be8f1
hjj007 <49516835+hjj007@users.noreply.github.com>
2019-04-12 13:54:24 +0800

    Update SuccApplication.java

--- src/main/java/com/baizhi/SuccApplication.java
+++ src/main/java/com/baizhi/SuccApplication.java
@@ -6,6 +6,24 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SuccApplication {
 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SuccApplication.class, args);
 	}
